51a0d7e5a7c19d6589b5309fb3fa6c4545659337
shikderbd24 <shikderplay@gmail.com>
2025-04-12 02:36:26 +0300

    login page logo updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,20 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="container mx-auto flex justify-center items-center h-screen">
+<body class="container mx-auto flex flex-col justify-center items-center h-screen">
     <h2 class="text-3xl font-bold text-center">
         Welcome to Dorjarpar.online
     </h2>
+    <button class="mt-4">
+        <a href="{{ route('login') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Login
+        </a>
+    </button>
+    <button class="mt-4">
+        <a href="{{ route('register') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Register
+        </a>
+    </button>
 </body>
 
 </html>
