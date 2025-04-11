0eb5134ec356a12b1914403a28780aaf4d8172ed
shikderbd24 <shikderplay@gmail.com>
2025-04-12 00:52:16 +0300

    addid all the required files

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dorjarpar.online | a website make by sohag hossain</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="container mx-auto flex justify-center items-center h-screen">
+    <h2 class="text-3xl font-bold text-center">
+        Welcome to Dorjarpar.online
+    </h2>
+</body>
+
+</html>
