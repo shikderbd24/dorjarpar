e71e903736c997b91239978c22721c21c8040b93
shikderbd24 <shikderplay@gmail.com>
2025-04-12 01:11:07 +0300

    Modified homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
 <body class="container mx-auto flex justify-center items-center h-screen">
     <h2 class="text-3xl font-bold text-center">
         Welcome to Dorjarpar.online
+        <p>This website is owned by <a href="https://shikderbd.org" target="_blank">Sohag Hossain</a></p>
     </h2>
 </body>
 
