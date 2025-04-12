021c7d19abd64c3b183c6a03d9c4a794a7156909
shikderbd24 <shikderplay@gmail.com>
2025-04-12 03:35:05 +0300

    logo related problems solved

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,1 @@
-<img src="logo.svg" width="100" alt="logo" /> <br>
-<h2 class="font-semibold text-xl text-gray-800 leading-tight">Dorjarpar.Online</h2>
+<img src="logo.svg" width="100" alt="logo" />
